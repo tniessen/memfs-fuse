c971d01a93da0c490a168559545e97032f6c41f7
Tobias Nießen <tniessen@tnie.de>
2016-10-01 19:32:12 +0200

    Improve debugging script

--- debug.sh
+++ debug.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 set -e
+
+dir=${1:?"Usage: $0 <mountpoint>"}
+
+if [ ! -d "$dir" ]; then
+  mkdir -p "$dir"
+fi
+
 make
-gdb --args ./memfs -d -s -o default_permissions -o auto_unmount testmount
+gdb --args ./memfs -d -s -o default_permissions -o auto_unmount "$dir"
+
